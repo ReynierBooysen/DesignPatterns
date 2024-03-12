fa559ef01466ae207261ec454a49ed5f530d37b4
ReynierBooysen <110087978+ReynierBooysen@users.noreply.github.com>
2024-03-12 16:05:56 +0100

    Threadsafe Singleton (#2)
    Co-authored-by: Reynier Booysen <reynier.booysen@friss.com>

---
+++ Singleton/Singleton.ThreadSafe/Counter.cs
@@ -0,0 +1,34 @@
+﻿namespace Singleton.ThreadSafe;
+
+/// <summary>
+/// Simple singleton class
+/// </summary>
+public class Counter
+{
+    /// <summary>
+    /// Creates a new instance of <see cref="Counter"/>
+    /// </summary>
+    /// <remarks>Constructor is protected so that class can be extended but not instantiated</remarks>
+    protected Counter() { }
+
+    /// <summary>
+    /// Returns the count with a private setter
+    /// </summary>
+    public int Count { get; private set; } = 0;
+
+    /// <summary>
+    /// Instance of Lazy of <see cref="Counter"/>
+    /// </summary>
+    private static readonly Lazy<Counter> _instance = new(() => new Counter());
+
+    /// <summary>
+    /// Returns the same/single instance of this class
+    /// </summary>
+    public static Counter Instance => _instance.Value;
+
+    /// <summary>
+    /// The singleton operation
+    /// </summary>
+    public void Add() => Count++;
+
+}
