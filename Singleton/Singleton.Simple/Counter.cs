df16e91d22642805800ed7e7f166d56680c6f107
Reynier Booysen <reynier.booysen@friss.com>
2024-03-12 15:49:58 +0100

    Simple Singleton

---
+++ Singleton/Singleton.Simple/Counter.cs
@@ -0,0 +1,44 @@
+﻿namespace Singleton.Simple;
+
+/// <summary>
+/// Simple singleton class
+/// </summary>
+public class Counter
+{
+    /// <summary>
+    /// Creates a new instance of <see cref="Counter"/>
+    /// </summary>
+    /// <remarks>Constructor is protected so that class can be extended but not instantiated</remarks>
+    protected Counter() { }
+
+    /// <summary>
+    /// Returns the count with a private setter
+    /// </summary>
+    public int Count { get; private set; } = 0;
+
+    /// <summary>
+    /// Nullable backing field so that we can check if the instance is null
+    /// </summary>
+    private static Counter? _instance;
+
+    /// <summary>
+    /// Returns the same/single instance of this class
+    /// </summary>
+    public static Counter Instance
+    {
+        get
+        {
+            if (_instance is null)
+            {
+                _instance = new Counter();
+            }
+            return _instance;
+        }
+    }
+
+    /// <summary>
+    /// The singleton operation
+    /// </summary>
+    public void Add() => Count++;
+
+}
--- Singleton/Singleton.Simple/Program.cs
+++ Singleton/Singleton.Simple/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Singleton.Simple;
+
+Console.Title = "Singleton.Simple";
+
+// Get two "instances"
+var instance1 = Counter.Instance;
+var instance2 = Counter.Instance;
+
+// Check if the instances are actually the same instance
+if (instance1 == instance2 
+    && instance1 == Counter.Instance
+    && instance2 == Counter.Instance)
+{
+    Console.WriteLine("They are all the same instance!");
+}
+Console.WriteLine();
+
+// Execute Singleton Operation from one instance and read from the other
+Console.WriteLine($"Execute singleton operation from {nameof(instance1)}");
+instance1.Add();
+Console.WriteLine($"The count is from {nameof(instance2)}: {instance2.Count}");
+Console.WriteLine();
+
+Console.WriteLine($"Execute singleton operation from {nameof(instance2)}");
+instance2.Add();
+Console.WriteLine($"The count is from {nameof(instance1)}: {instance1.Count}");
+
+Console.ReadLine();
