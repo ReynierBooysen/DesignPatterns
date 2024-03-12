61592f65319175e0c5c342c9dd6a7dae73841dd4
ReynierBooysen <110087978+ReynierBooysen@users.noreply.github.com>
2024-03-12 16:54:53 +0100

    IoC Singleton (#3)
    Co-authored-by: Reynier Booysen <reynier.booysen@friss.com>

---
+++ Singleton/Singleton.IoC/Counter.cs
@@ -0,0 +1,19 @@
+﻿namespace Singleton.IoC;
+
+/// <summary>
+/// Thread safe singleton class
+/// </summary>
+public class Counter
+{
+    /// <summary>
+    /// Returns the count with a private setter
+    /// </summary>
+    public int Count { get; private set; } = 0;
+
+    /// <summary>
+    /// The singleton operation
+    /// </summary>
+    public void Add() => Count++;
+
+}
+
---
+++ Singleton/Singleton.IoC/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Singleton.IoC;
+
+Console.Title = "Singleton.IoC";
+
+// Setup IoC and register service
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddSingleton<Counter>();
+using IHost host = builder.Build();
+
+Counter instance1 = host.Services.GetService<Counter>()!;
+Counter instance2 = host.Services.GetService<Counter>()!;
+
+// Check if the instances are actually the same instance
+if (instance1 == instance2)
+{
+    Console.WriteLine("They are the same instance!");
+}
+Console.WriteLine();
+
+// Execute Singleton Operation from one instance and read from the other
+Console.WriteLine($"Execute singleton operation from {nameof(instance1)}");
+instance1.Add();
+Console.WriteLine($"The count is from {nameof(instance2)}: {instance2.Count}");
+Console.WriteLine();
+
+Console.WriteLine($"Execute singleton operation from {nameof(instance2)}");
+instance2.Add();
+Console.WriteLine($"The count is from {nameof(instance1)}: {instance1.Count}");
+
+Console.ReadLine();
